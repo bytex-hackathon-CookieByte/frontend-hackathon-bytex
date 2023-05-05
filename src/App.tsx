bd88bbd227b8bd952bcd2a7889aff8dc720b789f
victorgrosu <vagrosu@yahoo.com>
2023-05-06 00:03:54 +0300

    Remove unnecessary files and add Toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "antd";
+import { toast } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button onClick={() => toast.success("Hello")}>Click me</Button>
     </div>
   );
 }
