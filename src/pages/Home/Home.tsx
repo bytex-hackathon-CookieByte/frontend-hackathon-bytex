6b0c7f7b58e1462d214413f07819585bedcfa5c2
Tudor Stroescu <tudorstroescu@yahoo.com>
2023-05-06 16:00:51 +0300

    added login and register forms

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   return (
-    <div className={"text-center"}>
+    <div className="text-center">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button onClick={() => toast.success("Hello")}>
         <FontAwesomeIcon icon={faHouse} className={"mr-1"} />
