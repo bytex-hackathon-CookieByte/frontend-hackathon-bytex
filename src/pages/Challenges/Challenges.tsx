80058d58d4d94e0b3dc00c9aa4cb0be4c2ab7c09
Tudor Stroescu <tudorstroescu@yahoo.com>
2023-05-07 01:33:22 +0300

    get and post on challenges cards

--- src/pages/Challenges/Challenges.tsx
+++ src/pages/Challenges/Challenges.tsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChallengeCard from "./components/ChallengeCard";
 import bytex_cover from "../../images/bytex-cover.png";
 import { Row } from "antd";
 import axios from "axios";
 
 function Challenges() {
-  async function fetchChallenges() {
-    try {
-      const response = await axios.get("http://localhost:8080/companies", {
-        params: {
-          username: "bytex_technologies",
-        },
-      });
-      const data = response.data;
-      console.log(data.entries);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  const [challenges, setChallenges] = useState<any>([]);
+
+  const fetchChallenges = () => {
+    axios({
+      url: "http://localhost:8080/challenges",
+      method: "GET",
+    })
+      .then((res) => {
+        console.log(res.data);
+        setChallenges(res.data);
+      })
+      .catch((err) => {});
+  };
 
   useEffect(() => {
     fetchChallenges();
@@ -30,36 +30,13 @@ function Challenges() {
         justify={"center"}
         className="max-w-full"
       >
-        <ChallengeCard
-          title="Bytex Frontend Challenge"
-          description="lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
-          imgSrc={bytex_cover}
-        />
-        <ChallengeCard
-          title="Bytex Backend Challenge"
-          description="lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
-          imgSrc={bytex_cover}
-        />
-        <ChallengeCard
-          title="Bytex Testing Challenge"
-          description="lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
-          imgSrc={bytex_cover}
-        />
-        <ChallengeCard
-          title="Bytex UI/UX Challenge"
-          description="lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
-          imgSrc={bytex_cover}
-        />
-        <ChallengeCard
-          title="Bytex QA Challenge"
-          description="lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
-          imgSrc={bytex_cover}
-        />
-        <ChallengeCard
-          title="Bytex DevOps Challenge"
-          description="lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
-          imgSrc={bytex_cover}
-        />
+        {challenges.map((challenge: any) => (
+          <ChallengeCard
+            title={challenge.title}
+            description={challenge.description}
+            imgSrc={bytex_cover}
+          />
+        ))}
       </Row>
     </div>
   );
--- src/pages/Challenges/CompanyChallenges.tsx
+++ src/pages/Challenges/CompanyChallenges.tsx
@@ -1,6 +1,49 @@
-import React, { useState } from "react";
-import { Card, Col, Row, Button } from "antd";
+import React, { useEffect, useState } from "react";
+import ChallengeCard from "./components/ChallengeCard";
+import bytex_cover from "../../images/bytex-cover.png";
+import { Row, Button } from "antd";
+import axios from "axios";
+import AddChallenge from "./components/AddChallenge";
 
-export default function CompanyChallenges() {
-  return <div></div>;
+function Challenges() {
+  const [challenges, setChallenges] = useState<any>([]);
+
+  const fetchChallenges = () => {
+    axios({
+      url: "http://localhost:8080/challenges",
+      method: "GET",
+    })
+      .then((res) => {
+        console.log(res.data);
+        setChallenges(res.data);
+      })
+      .catch((err) => {});
+  };
+
+  useEffect(() => {
+    fetchChallenges();
+  }, []);
+
+  return (
+    <>
+      <AddChallenge fetchChallenges={fetchChallenges} />
+      <div className="w-100">
+        <Row
+          gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+          justify={"center"}
+          className="max-w-full"
+        >
+          {challenges.map((challenge: any) => (
+            <ChallengeCard
+              title={challenge.title}
+              description={challenge.description}
+              imgSrc={bytex_cover}
+            />
+          ))}
+        </Row>
+      </div>
+    </>
+  );
 }
+
+export default Challenges;
