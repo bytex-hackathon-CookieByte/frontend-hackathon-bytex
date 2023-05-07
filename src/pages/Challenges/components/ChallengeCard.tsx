8d816730e6e85c6e6b033a19ea0c1549d0b87c40
Tudor Stroescu <tudorstroescu@yahoo.com>
2023-05-07 08:07:25 +0300

    Fixed button text for user/company

--- src/pages/Challenges/components/ChallengeCard.tsx
+++ src/pages/Challenges/components/ChallengeCard.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Avatar, Button, Card } from "antd";
 import { Col } from "antd";
 import DeleteChallenge from "./DeleteChallenge";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { UserContext } from "../../../context/UserContext";
 
 const { Meta } = Card;
 
@@ -17,6 +19,34 @@ export default function ChallengeCard(props: {
   //     points: 100,
   //   });
 
+  const { type, username, id } = useContext(UserContext);
+  const [myUsername, setMyUsername] = useState<string>(username);
+  const [myId, setMyId] = useState<string>(id);
+
+  const handleAddChallenge = () => {
+    setMyUsername(username);
+    setMyId(id);
+    console.log("id", myId, "username:", myUsername);
+    const challengeData = {
+      username: "tudstk",
+      challengeId: "2d862a23-3fde-483e-8dc5-7658ad25fef6",
+      scoreValue: 10,
+    };
+    console.log(challengeData);
+    axios
+      .post("http://localhost:8080/users/scores", challengeData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <Col className="gutter-row mt-5">
       <Card
@@ -24,15 +54,13 @@ export default function ChallengeCard(props: {
         cover={<img alt="example" src={props.imgSrc} />}
         actions={[
           <>
-            <Button>
-              {" "}
-              <Link to="/battle">Accept Challenge</Link>
+            <Button onClick={handleAddChallenge}>
+              {type === "company" ? (
+                <Link to="/battle">View Challenge</Link>
+              ) : (
+                <Link to="/battle">Accept Challenge</Link>
+              )}
             </Button>
-            <Button>
-              <Link to="/challenges/asfasf/stages">View</Link>
-            </Button>
-            {/* <Button>Delete Challenge</Button> */}
-            {/* <DeleteChallenge /> */}
           </>,
         ]}
       >
--- src/pages/MyChallenges/MyChallenges.tsx
+++ src/pages/MyChallenges/MyChallenges.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import ChallengeCard from "../Challenges/components/ChallengeCard";
+import bytex_cover from "../../images/bytex-cover.png";
+import { Row } from "antd";
+import { UserContext } from "../../context/UserContext";
 
 function MyChallenges() {
-  return <div>mychallenges</div>;
+  const [challenges, setChallenges] = useState<any>([]);
+  const { id } = useContext(UserContext);
+  const [myId, setMyId] = useState<string>(id);
+
+  const fetchChallenges = () => {
+    console.log("MY ID", id);
+    axios({
+      url: `http://localhost:8080/users/scores?userId=${myId}`,
+      method: "GET",
+    })
+      .then((res) => {
+        setChallenges(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchChallenges();
+  }, []);
+
+  return (
+    <div className="w-100">
+      <Row
+        gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
+        justify={"center"}
+        className="max-w-full"
+      >
+        {challenges.map((challenge: any) => (
+          <ChallengeCard
+            title={challenge.challenge.title}
+            description={challenge.challenge.description}
+            imgSrc={bytex_cover}
+          />
+        ))}
+      </Row>
+    </div>
+  );
 }
 
 export default MyChallenges;
